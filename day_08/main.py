f58a3707ce85cf14513f21d68a4c4cc00f5ed14c
BeatButton <thebeatbutton@gmail.com>
2021-12-08 11:06:15 -0700

    cleanup day 8

--- day_08/main.py
+++ day_08/main.py
@@ -4,20 +4,6 @@ from itertools import permutations
 with open(Path(__file__).with_name("input")) as fp:
     inp = fp.read().strip()
 
-# fmt: off
-_canonical_digits = [
-    [0, 1, 2, 4, 5, 6],    # 0
-    [2, 5],                # 1
-    [0, 2, 3, 4, 6],       # 2
-    [0, 2, 3, 5, 6],       # 3
-    [1, 2, 3, 5],          # 4
-    [0, 1, 3, 5, 6],       # 5
-    [0, 1, 3, 4, 5, 6],    # 6
-    [0, 2, 5],             # 7
-    [0, 1, 2, 3, 4, 5, 6], # 8
-    [0, 1, 2, 3, 5, 6],    # 9
-]
-# fmt: on
 canonical_digits = [
     "abcefg",
     "cf",
@@ -33,21 +19,18 @@ canonical_digits = [
 
 ans = 0
 for line in inp.splitlines():
-    left, right = map(str.strip, line.split("|"))
+    left, right = map(str.split, line.split("|"))
     for guess in permutations("abcdefg"):
         trans = str.maketrans("".join(guess), "abcdefg")
-        for i, digit in enumerate(left.split()):
+        for digit in left:
             t_digit = "".join(sorted(digit.translate(trans)))
             if t_digit not in canonical_digits:
                 break
-
         else:
-            this_line = 0
-            for idx, digit in enumerate(right.split()[::-1]):
+            for idx, digit in enumerate(right[::-1]):
                 t_digit = "".join(sorted(digit.translate(trans)))
-                this_line += canonical_digits.index(t_digit) * 10 ** idx
-
-            ans += this_line
+                ans += canonical_digits.index(t_digit) * 10 ** idx
+            break
 
 
 print(ans)

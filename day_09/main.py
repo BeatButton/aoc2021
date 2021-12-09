e48f4840f3e01f8e3321dc1f26fa028921631b93
BeatButton <thebeatbutton@gmail.com>
2021-12-08 22:23:15 -0700

    day 9 part 2

--- day_09/main.py
+++ day_09/main.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from functools import reduce
+from operator import mul
 
 with open(Path(__file__).with_name("input")) as fp:
     inp = fp.read().strip()
@@ -14,19 +16,31 @@ h = len(data)
 def get_surrounding(x, y):
     return filter(
         (lambda p: p[0] >= 0 and p[1] >= 0 and p[1] < w and p[0] < h),
-        [[x + 1, y], [x - 1, y], [x, y - 1], [x, y + 1]],
+        [(x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)],
     )
 
 
-def is_risk(x, y):
+def is_low(x, y):
     return all(data[nx][ny] > data[x][y] for nx, ny in get_surrounding(x, y))
 
 
-ans = 0
+basins = []
 for x, row in enumerate(data):
     for y, val in enumerate(row):
-        if is_risk(x, y):
-            ans += val + 1
+        if is_low(x, y):
+            if any((x, y) in basin for basin in basins):
+                print("odd!")
+                continue
+            basin = set()
+            check = [(x, y)]
+            while check:
+                cx, cy = check.pop()
+                basin.add((cx, cy))
+                for nx, ny in get_surrounding(cx, cy):
+                    if (nx, ny) not in basin and data[nx][ny] != 9:
+                        check.append((nx, ny))
+            basins.append(basin)
 
 
+ans = reduce(mul, map(len, sorted(basins, key=len)[-3:]))
 print(ans)

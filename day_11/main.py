7056957d44dd49fa5fc4bf6d5fe0448ded31edd8
BeatButton <thebeatbutton@gmail.com>
2021-12-10 22:11:55 -0700

    day 11 part 1

---
+++ day_11/main.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+with open(Path(__file__).with_name("input")) as fp:
+    inp = fp.read().strip()
+
+es = [[int(c) for c in line] for line in inp.splitlines()]
+
+h = len(es)
+w = len(es[0])
+
+
+def get_surrounding(ox, oy):
+    for x in range(ox - 1, ox + 2):
+        for y in range(oy - 1, oy + 2):
+            if x >= 0 and y >= 0 and y < w and x < h and not (x == ox and y == oy):
+                yield x, y
+
+
+def flash(x, y):
+    if (x, y) in flashed:
+        return
+
+    flashed.add((x, y))
+    for x, y in get_surrounding(x, y):
+        es[x][y] += 1
+        if es[x][y] > 9:
+            flash(x, y)
+
+
+def step():
+    for x, row in enumerate(es):
+        for y, e in enumerate(row):
+            row[y] += 1
+            if row[y] > 9:
+                flash(x, y)
+
+
+flashed = set()
+
+ans = 0
+for i in range(100):
+    step()
+    ans += len(flashed)
+    for x, y in flashed:
+        es[x][y] = 0
+    flashed = set()
+
+print(ans)

c02a117a1bb4071333b8d9a6152e24385dc99eea
BeatButton <thebeatbutton@gmail.com>
2021-12-15 00:56:49 -0700

    day 15 part 2

--- day_15/main.py
+++ day_15/main.py
@@ -14,25 +14,49 @@ def adj(xy):
 
 grid = {}
 dist = {}
-unvisited = []
 for y, line in enumerate(inp.splitlines()):
     for x, cost in enumerate(map(int, line)):
         grid[x, y] = cost
         dist[x, y] = INF
-        unvisited.append((x, y))
 
 dist[0, 0] = 0
-unvisited[:] = unvisited[1:] + [(0, 0)]  # lol
 
+mx = my = inp.count("\n")
+mx += 1
+my += 1
+# horiz
+for dy in range(1, 5):
+    for x in range(mx):
+        for y in range(my * dy, my * (dy + 1)):
+            grid[x, y] = (grid[x, y - my]) % 9 + 1
+            dist[x, y] = INF
+
+# vert
+for dx in range(1, 5):
+    for y in range(my):
+        for x in range(mx * dx, mx * (dx + 1)):
+            grid[x, y] = (grid[x - mx, y]) % 9 + 1
+            dist[x, y] = INF
+
+# rest
+for dx in range(1, 5):
+    for dy in range(1, 5):
+        for x in range(mx * dx, mx * (dx + 1)):
+            for y in range(my * dy, my * (dy + 1)):
+                grid[x, y] = (grid[x, y - my]) % 9 + 1
+                dist[x, y] = INF
+
+unvisited = [(0, (0, 0))]
 while unvisited:
-    curr = unvisited.pop()
+    dst, curr = heapq.heappop(unvisited)
     for nxt in adj(curr):
         try:
             cost = grid[nxt]
         except KeyError:
             continue
-        dist[nxt] = min(dist[nxt], dist[curr] + cost)
-    unvisited.sort(key=lambda p: dist[p], reverse=True)
-
+        d = dist[curr] + cost
+        if d < dist[nxt]:
+            dist[nxt] = d
+            heapq.heappush(unvisited, (d, nxt))
 
 print(dist[max(grid)])

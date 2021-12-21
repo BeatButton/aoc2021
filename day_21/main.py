491d75a40bf2c969c42c458ab74a135789213952
BeatButton <thebeatbutton@gmail.com>
2021-12-20 22:37:26 -0700

    day 21 part 2

--- day_21/main.py
+++ day_21/main.py
@@ -1,36 +1,85 @@
 from pathlib import Path
-from itertools import cycle
+from functools import lru_cache
+
+
+def start(p1, p2):
+    return (
+        roll11(p1, 0, p2, 0, r=1)
+        + roll11(p1, 0, p2, 0, r=2)
+        + roll11(p1, 0, p2, 0, r=3)
+    )
+
+
+@lru_cache(maxsize=None)
+def roll11(p1, s1, p2, s2, r):
+    return (
+        roll12(p1, s1, p2, s2, r=r + 1)
+        + roll12(p1, s1, p2, s2, r=r + 2)
+        + roll12(p1, s1, p2, s2, r=r + 3)
+    )
+
+
+@lru_cache(maxsize=None)
+def roll12(p1, s1, p2, s2, r):
+    return (
+        roll13(p1, s1, p2, s2, r=r + 1)
+        + roll13(p1, s1, p2, s2, r=r + 2)
+        + roll13(p1, s1, p2, s2, r=r + 3)
+    )
+
+
+@lru_cache(maxsize=None)
+def roll13(p1, s1, p2, s2, r):
+    p1 += r
+    p1 = (p1 - 1) % 10 + 1
+    s1 += p1
+    if s1 >= 21:
+        return 1
+    return (
+        roll21(p1, s1, p2, s2, r=1)
+        + roll21(p1, s1, p2, s2, r=2)
+        + roll21(p1, s1, p2, s2, r=3)
+    )
+
+
+@lru_cache(maxsize=None)
+def roll21(p1, s1, p2, s2, r):
+    return (
+        roll22(p1, s1, p2, s2, r=r + 1)
+        + roll22(p1, s1, p2, s2, r=r + 2)
+        + roll22(p1, s1, p2, s2, r=r + 3)
+    )
+
+
+@lru_cache(maxsize=None)
+def roll22(p1, s1, p2, s2, r):
+    return (
+        roll23(p1, s1, p2, s2, r=r + 1)
+        + roll23(p1, s1, p2, s2, r=r + 2)
+        + roll23(p1, s1, p2, s2, r=r + 3)
+    )
+
+
+@lru_cache(maxsize=None)
+def roll23(p1, s1, p2, s2, r):
+    p2 += r
+    p2 = (p2 - 1) % 10 + 1
+    s2 += p2
+    if s2 >= 21:
+        return 1j
+    return (
+        roll11(p1, s1, p2, s2, r=1)
+        + roll11(p1, s1, p2, s2, r=2)
+        + roll11(p1, s1, p2, s2, r=3)
+    )
+
 
 with open(Path(__file__).with_name("input")) as fp:
     inp = fp.read().strip()
 
 lines = iter(inp.splitlines())
-p1 = int(next(lines)[-1])
-s1 = 0
-p2 = int(next(lines)[-1])
-s2 = 0
-die = cycle(range(1, 101))
+a = int(next(lines)[-1])
+b = int(next(lines)[-1])
 
-c = 0
-while True:
-    for _ in range(3):
-        p1 += next(die)
-        c += 1
-    p1 = (p1 - 1) % 10 + 1
-    s1 += p1
-
-    if s1 >= 1000:
-        break
-
-    for _ in range(3):
-        p2 += next(die)
-        c += 1
-    p2 = (p2 - 1) % 10 + 1
-    s2 += p2
-
-    if s2 >= 1000:
-        break
-    print(s1)
-    print(s2)
-
-print(min(s1, s2) * c)
+ans = start(a, b)
+print(max(ans.imag, ans.real))

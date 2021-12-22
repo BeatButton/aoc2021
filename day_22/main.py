8fc736cb03e83ba2017ad1ad0388845d8e9411a3
BeatButton <thebeatbutton@gmail.com>
2021-12-21 22:22:43 -0700

    day 22 part 2 try 1

--- day_22/main.py
+++ day_22/main.py
@@ -1,10 +1,11 @@
 from pathlib import Path
-from collections import defaultdict
 
 with open(Path(__file__).with_name("input")) as fp:
     inp = fp.read().strip()
 
-cube = defaultdict(int)
+ranges = []
+
+minx = maxx = miny = maxy = minz = maxz = 0
 
 for line in inp.splitlines():
     inst, args = line.split()
@@ -14,11 +15,27 @@ for line in inp.splitlines():
         for arg in args.split(",")
     )
     xarg, yarg, zarg = args
-    if any(arg.start < -50 or arg.start > 50 for arg in args):
-        continue
-    for x in xarg:
-        for y in yarg:
-            for z in zarg:
-                cube[x, y, z] = on
 
-print(sum(cube.values()))
+    minx = min(minx, xarg.start)
+    maxx = max(maxx, xarg.stop)
+    miny = min(miny, yarg.start)
+    maxy = max(maxy, yarg.stop)
+    minz = min(minz, zarg.start)
+    maxz = max(maxz, zarg.stop)
+
+    ranges.append((args, on))
+
+ans = 0
+print(minx, maxx)
+print(miny, maxy)
+for x in range(minx, maxy):
+    print(f"x = {x}")
+    for y in range(miny, maxy):
+        print(f"y = {y}")
+        for z in range(minz, maxz):
+            for (xr, yr, zr), on in ranges[::-1]:
+                if x in xr and y in yr and z in zr:
+                    ans += on
+                    break
+
+print(ans)

4c3f3d7a12c2017687b8db739b9f4a97aee0905c
BeatButton <thebeatbutton@gmail.com>
2021-12-21 23:21:17 -0700

    day 22 part 2 try 2

--- day_22/main.py
+++ day_22/main.py
@@ -1,13 +1,78 @@
+from collections import defaultdict
 from pathlib import Path
 
-with open(Path(__file__).with_name("input")) as fp:
+with open(Path(__file__).with_name("test3")) as fp:
     inp = fp.read().strip()
 
-ranges = []
 
-minx = maxx = miny = maxy = minz = maxz = 0
+cols = defaultdict(list)
 
+
+def intersecting(ranges, nr):
+    out = []
+    for i, (r, on) in enumerate(ranges):
+        if nr.stop <= r.start:
+            continue
+        if r.stop <= nr.start:
+            continue
+        out.append((i, (r, on)))
+    return out
+
+
+def contains(r1, r2):
+    return r1.start <= r2.start and r1.stop >= r2.stop
+
+
+def merge(ranges, nr, non):
+    offset = 0
+    intersects = intersecting(ranges, nr)
+    if not intersects:
+        ranges.append((nr, non))
+        ranges.sort(key=lambda r: r[0].start)
+        return
+    added = False
+    for i, (r, on) in intersects:
+        if contains(nr, r):
+            del ranges[i + offset]
+            offset -= 1
+        elif contains(r, nr):
+            if on != non:
+                before = range(r.start, nr.start), on
+                after = range(nr.stop, r.stop), on
+                ranges[i + offset : i + offset + 1] = [before, (nr, non), after]
+            added = True
+            break
+        elif nr.stop > r.start:
+            if non == on:
+                new = range(nr.start, r.stop), on
+                ranges[i + offset] = new
+            else:
+                replace = range(nr.stop, r.stop), on
+                ranges[i + offset : i + offset + 1] = [(nr, non), replace]
+            added = True
+            break
+        elif nr.start < r.stop:
+            added = True
+            if non == on:
+                new = range(r.start, nr.stop), on
+                ranges[i + offset] = new
+            else:
+                replace = range(r.start, nr.start), on
+                ranges[i + offset : i + offset + 1] = [replace, (nr, non)]
+                offset += 1
+                if (r, on) != intersects[-1]:
+                    print("WARN: BUTTHOLE")
+        if not added:
+            ranges.append((nr, non))
+            ranges.sort(key=lambda r: r[0].start)
+
+
+i = 0
 for line in inp.splitlines():
+    i += 1
+    print(i)
+    if i == 11:
+        breakpoint()
     inst, args = line.split()
     on = inst == "on"
     args = tuple(
@@ -16,26 +81,18 @@ for line in inp.splitlines():
     )
     xarg, yarg, zarg = args
 
-    minx = min(minx, xarg.start)
-    maxx = max(maxx, xarg.stop)
-    miny = min(miny, yarg.start)
-    maxy = max(maxy, yarg.stop)
-    minz = min(minz, zarg.start)
-    maxz = max(maxz, zarg.stop)
-
-    ranges.append((args, on))
+    for x in xarg:
+        for y in yarg:
+            merge(cols[x, y], zarg, on)
 
 ans = 0
-print(minx, maxx)
-print(miny, maxy)
-for x in range(minx, maxy):
-    print(f"x = {x}")
-    for y in range(miny, maxy):
-        print(f"y = {y}")
-        for z in range(minz, maxz):
-            for (xr, yr, zr), on in ranges[::-1]:
-                if x in xr and y in yr and z in zr:
-                    ans += on
-                    break
+print(len(cols))
+i = 0
+for col in cols.values():
+    i += 1
+    print(i)
+    for r, on in col:
+        if on:
+            ans += len(r)
 
 print(ans)
